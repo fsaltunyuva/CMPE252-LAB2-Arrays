78e7c87d4cd6f4149e3b85565c40707d397dc603
Furkan Safa Altunyuva <furkansafaaltunyuva@gmail.com>
2023-05-03 17:04:53 +0300

    Correct VPL

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/user/CLionProjects/CMPE252-Lab2-Q1
+# Install script for directory: C:/Users/user/Desktop/GitHub/CMPE252-Lab2-Q1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/user/CLionProjects/CMPE252-Lab2-Q1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/user/Desktop/GitHub/CMPE252-Lab2-Q1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 
-#define SIZE 100
+#define SIZE 1000
 
 void printNumbers(const int arr[], int n){
     printf("Array elements: ");
@@ -21,36 +21,6 @@ void readInput(int arr[], int *nPtr){
     }
 }
 
-void findLeaders(const int arr[], int n, int leadersArr[], int *sp)
-{
-    int leaderArrayCurrentIndex = 1; //Çünkü sonuncu eleman her zaman ekli başlayacak
-    leadersArr[0] = arr[n-1];
-
-    for(int i = 0; i < n-1; i++){ //n-1'e kadar çünkü zaten sonuncu ekli
-        int greaterThanAll = 0;
-        for(int j = i+1; j < n-1; j++){ //n-1'e kadar çünkü zaten sonuncu ekli
-            greaterThanAll = 0;
-            if(arr[i] > arr[j]){
-                greaterThanAll = 1;
-            }
-            else{
-                greaterThanAll = 0;
-            }
-        }
-        if(greaterThanAll == 1){
-            leadersArr[leaderArrayCurrentIndex] = arr[i];
-            leaderArrayCurrentIndex++;
-        }
-    }
-
-    printf("Leaders Array elements: \n");
-
-    for(int i = 0; i < n; i++){
-        if(isspace(leadersArr[i]) == 0){
-            printf("%d ", leadersArr[i]);
-        }
-    }
-}
 
 int main() {
     int arr[SIZE];
@@ -58,14 +28,6 @@ int main() {
 
     readInput(arr, &n);
     printNumbers(arr, n);
-
-    printf("\nMinimum of array is: %d", min);
-    printf("\nMaximum of array is: %d", max);
-
-    int leadersArr[SIZE];
-    int size2 = 100;
-
-    findLeaders(arr, n, leadersArr, &size2);
 
     return 0;
 }
